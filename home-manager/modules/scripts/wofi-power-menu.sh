df2192e401a07b2f0b32255d31fdffcf23499679
WaterIris <179727984+WaterIris@users.noreply.github.com>
2025-07-13 15:05:40 +0200

    Add cleanup before recolor

--- home-manager/modules/scripts/wofi-power-menu.sh
+++ home-manager/modules/scripts/wofi-power-menu.sh
@@ -1,14 +1,18 @@
 # Define power options
 options="Shutdown\nReboot\nSuspend"
-wofi_width=200
+wofi_width=240
+wofi_height=240
 
 # Show the menu with wofi
 choice=$(echo -e "$options" | wofi --dmenu \
   --prompt "Power Menu" \
-  --lines 5 \
+  --lines 6 \
   --width "$wofi_width" \
+  --height "$wofi_height" \
   --no-persist \
   --allow-markup \
+  --location "top_right" \
+  --xoffset -10 \
   --hide-scroll)
 
 # Act based on user choice
